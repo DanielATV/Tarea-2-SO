f3af259f25246a756904a327970184b2f4b6c615
Jorge Díaz <32210150+jorgedz17@users.noreply.github.com>
2019-12-30 00:47:23 -0300

    Add files via upload
    1/3 de la 2

--- Ejercicio2/Tarea2b/prueba/Main.java
+++ Ejercicio2/Tarea2b/prueba/Main.java
@@ -2,12 +2,15 @@ package prueba;
 
 public class Main{
 	public static void main(String[ ] args) {
-
-		Thread hilo = new Proceso("proceso 1");
-		Thread hilo2 = new Proceso("proceso 2");
-
-		hilo.start();
+		
+		int x[]= {1,4,2,10,6,7,9};
+		int value;
+		Thread hilo1 = new Proceso("proceso 1",x,0,(x.length/2)-1);
+		Thread hilo2 = new Proceso("proceso 2",x,x.length/2,x.length-1);
+		hilo1.start();
 		hilo2.start();
+		value=hilo1.j
+
 
 
 
--- Ejercicio2/Tarea2b/prueba/Proceso.java
+++ Ejercicio2/Tarea2b/prueba/Proceso.java
@@ -2,17 +2,46 @@ package prueba;
 
 public class Proceso extends Thread{
 
-	public Proceso(String msg){
-
+	int arreglo[];
+	int der;
+	int izq;
+	public Proceso(String msg,int array[],int indice_izquierdo, int indice_derecho){
 		super(msg);
+		arreglo=array;
+	    der=indice_derecho;
+		izq=indice_izquierdo;
 	} 
-
 	public void run(){
-
-		for (int i=0;i<10 ;i++ ) {
+		
+		if(this.izq == this.der) {/*Verificamos el tamaño del arreglo*/
+			System.out.println("valor: "+arreglo[izq]+ " posicion:  "+izq);
+			
+			
+		}else {/*Si no es de tamaño igual a uno, dividimos en dos thred la tarea*/
+			
+			int largo=(this.der - this.izq);
+			Thread hilo1 = new Proceso("subproceso 1",arreglo,this.izq,this.izq+largo/2);
+			Thread hilo2 = new Proceso("subproceso 2",arreglo,this.izq+largo/2+1,this.der);
+			hilo1.start();
+			hilo2.start();
+		}
+			
+			
+			
+		/*for (int i=0;i<10 ;i++ ) {
 			System.out.println(this.getName());			
 		}
-
+		
+		*/
+		
+	}
+	public int[] obtener_valores(int indice_derecho, int indice_izquierdo, int[] array){
+		int largo=indice_izquierdo - indice_derecho;
+		int x[]=new int[largo];
+		for(int i=0;i<largo;i++) {
+			x[i]=array[indice_derecho+i];
+		}
+		return x;
 	}
 	
 
