92e0646dc6fb11f194d11acd609814bff1739b9e
Daniel Toro <daniel.torov@sansano.usm.cl>
2020-01-11 21:04:10 -0300

    Input de usuario

---
+++ Tarea2a/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{ 
+	
+	public static void main(String args[]) {
+		// Input de usuario;
+		Scanner readerObj = new Scanner(System.in);
+   		System.out.println("Ingrese la funcion");
+    	String userName = readerObj.nextLine();  
+	
+	} 
+} 
